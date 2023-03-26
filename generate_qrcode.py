00e3a169113602d01a943c16652b77c8c650785c
shubhamvitthalrasal <shubhamrasal861@gmail.com>
2023-03-26 18:13:12 +0530

    added modify generate qrcode file

---
+++ generate_qrcode.py
@@ -0,0 +1,21 @@
+
+# Importing library
+import qrcode
+ 
+# Data to encode
+data = "www.google.com"
+ 
+# Creating an instance of QRCode class
+qr = qrcode.QRCode(version = 1,
+                   box_size = 10,
+                   border = 5)
+ 
+# Adding data to the instance 'qr'
+qr.add_data(data)
+ 
+qr.make(fit = True)
+img = qr.make_image(fill_color = 'red',
+                    back_color = 'white')
+ 
+img.save('MyQRCode2.png')
+
